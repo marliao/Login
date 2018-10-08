1cff3d64adbf2c2ea1fd9cd27dfd96927d1037ab
marliao <892564305@qq.com>
2018-10-08 19:19:48 +0800

    QQ界面登录功能（未完成）

--- app/src/main/java/com/itheima/cn/qqlogin/MainActivity.java
+++ app/src/main/java/com/itheima/cn/qqlogin/MainActivity.java
@@ -1,20 +1,25 @@
 package com.itheima.cn.qqlogin;
 
 import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText et_username;
     private EditText et_password;
     private CheckBox cb_remberMemory;
+    private TextView tv_login1;
+    private TextView tv_login2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
         et_username=findViewById(R.id.et_username);
         et_password = findViewById(R.id.et_password);
         cb_remberMemory = findViewById(R.id.cb_remberMemory);
+        tv_login1=findViewById(R.id.tv_login1);
+        tv_login2=findViewById(R.id.tv_login2);
 
         //        //获取保存的用户名密码
 //        Map<String,String> maps=UserInforUtils.readInfor(MainActivity.this);
@@ -43,6 +50,9 @@ public class MainActivity extends AppCompatActivity {
         et_password.setText(password);
         cb_remberMemory.setChecked(ischeck);
 //    }
+
+        tv_login1.setOnClickListener(this);
+        tv_login2.setOnClickListener(this);
 
     }
 
@@ -79,4 +89,14 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onClick(View v) {
+        Drawable login1=tv_login1.getBackground();
+        Drawable login2=tv_login2.getBackground();
+        Drawable colorChange=null;
+        colorChange=login1;
+        login1=login2;
+        login2=colorChange;
+
+    }
 }
